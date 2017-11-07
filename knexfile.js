e8526a32da258277e1e86b22ae9c32a2dcc08c58
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-07 11:37:07 -0800

    Prepare for heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,15 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, 'src', 'db', 'seeds')
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
+    }, 
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
+    }
   }
 }
