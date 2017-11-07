b01ff83e4a8b273f9640b88f9c3d5e1cc5892b83
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-07 11:39:17 -0800

    fix typo

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations')
     }, 
